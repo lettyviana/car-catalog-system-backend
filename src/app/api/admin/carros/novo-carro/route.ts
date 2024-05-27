4b6f7f86237486f8334f9dea6138b3fdd2a7e410
lettyviana <letisviana@gmail.com>
2024-05-27 09:00:32 -0300

    feat: Created route to register a new car

---
+++ src/app/api/admin/carros/novo-carro/route.ts
@@ -0,0 +1,78 @@
+import { verifyToken } from "@/lib/authentication";
+import connectToDatabase from "@/lib/dbConfig/connectToDatabase";
+import { Response } from "@/lib/helpers/standardMessage";
+import { CarModel } from "@/lib/models/carModel";
+import { NextRequest } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    await connectToDatabase();
+    
+    const decoded = await verifyToken(request);
+    const { adminId } = decoded;
+    const { name, make, model, photo, price, year, version } =
+      await request.json();
+
+    if (!name || !make || !model || !photo) {
+      return Response({
+        object: {
+          error: "Preencha os dados para cadastrar o veículo.",
+          success: false,
+        },
+        status: 400,
+      });
+    }
+
+    const existingCar = await CarModel.findOne({ name });
+
+    if (existingCar) {
+      return Response({
+        object: {
+          error: "Já existe um veículo com esse nome.",
+          success: false,
+        },
+        status: 400,
+      });
+    }
+
+    try {
+      const createdCar = new CarModel({
+        name,
+        make,
+        model,
+        photo,
+        price,
+        year,
+        version,
+        adminId,
+      });
+
+      await createdCar.save();
+
+      return Response({
+        object: {
+          error: null,
+          message: "Veículo cadastrado com sucesso!",
+          success: true,
+        },
+        status: 200,
+      });
+    } catch (error) {
+      return Response({
+        object: {
+          error: `Erro ao cadastrar o veículo: ${error}`,
+          success: false,
+        },
+        status: 500,
+      });
+    }
+  } catch (error: any) {
+    return Response({
+      object: {
+        error: error.message,
+        success: false,
+      },
+      status: 500,
+    });
+  }
+}
