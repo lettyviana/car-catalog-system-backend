cb379187517c999560154d2bc63adb5fb98aaae7
lettyviana <letisviana@gmail.com>
2024-05-27 21:03:38 -0300

    feat: Created route for getting the cars registered by the logged admin

---
+++ src/app/api/admin/carros/route.ts
@@ -0,0 +1,50 @@
+import connectToDatabase from "@/lib/dbConfig/connectToDatabase";
+import { verifyToken } from "@/lib/authentication";
+import { Response } from "@/lib/helpers/standardMessage";
+import { CarModel } from "@/lib/models/carModel";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  try {
+    await connectToDatabase();
+
+    const decoded = await verifyToken(request);
+
+    if (decoded.status) {
+      return decoded;
+    }
+
+    const { adminId } = decoded;
+
+    const cars = await CarModel.find({ adminId });
+
+    if (!cars) {
+      return Response({
+        object: {
+          error: "Nenhum veículo encontrado.",
+          success: false,
+        },
+        status: 404,
+      });
+    }
+
+    return Response({
+      object: {
+        error: null,
+        message: `Veículos encontrados do admin ${adminId}.`,
+        cars: cars,
+        success: true,
+      },
+      status: 200,
+    });
+  } catch (error: any) {
+    return Response({
+      object: {
+        error: error.message,
+        success: false,
+      },
+      status: 501,
+    });
+  }
+}
+
