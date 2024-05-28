92c1881ab2b19f270ccbb287cb023c4a81be6aca
lettyviana <letisviana@gmail.com>
2024-05-28 09:58:01 -0300

    feat: Created route for updating car data

---
+++ src/app/api/admin/carros/[id]/route.ts
@@ -0,0 +1,54 @@
+import connectToDatabase from "@/lib/dbConfig/connectToDatabase";
+import { verifyToken } from "@/lib/authentication";
+import { Response } from "@/lib/helpers/standardMessage";
+import { CarModel } from "@/lib/models/carModel";
+import { NextRequest } from "next/server";
+
+connectToDatabase();
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const decoded = await verifyToken(request);
+
+    if (decoded.status) {
+      return decoded;
+    }
+
+    const { id } = params;
+    const { name, make, model, photo, price, year, version } =
+      await request.json();
+
+    try {
+      const carData = { name, make, model, photo, price, year, version };
+      await CarModel.findByIdAndUpdate(id, carData);
+
+      return Response({
+        object: {
+          error: null,
+          message: "Dados do veículo atualizados!",
+          success: true,
+        },
+        status: 200,
+      });
+    } catch (error: any) {
+      return Response({
+        object: {
+          error: error.message,
+          success: false,
+        },
+        status: 500,
+      });
+    }
+  } catch (error: any) {
+    return Response({
+      object: {
+        error: "Não foi possível atualizar os dados.",
+        success: false,
+      },
+      status: 500,
+    });
+  }
+}
